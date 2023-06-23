eaeba5ec8d8a9b58536efdfd97b02c84dd359031
Luís Fonseca <luis@lhf.pt>
2023-06-23 12:29:37 +0100

    feat: add indexer tool

--- flake.nix
+++ flake.nix
@@ -3,12 +3,49 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
+
     utils.url = "github:numtide/flake-utils";
+
+    nix-filter.url = "github:numtide/nix-filter";
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+    };
+
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "utils";
+      inputs.rust-overlay.follows = "rust-overlay";
+    };
   };
 
   outputs = { ... } @ inputs: inputs.utils.lib.eachDefaultSystem (system:
     let
-      pkgs = import inputs.nixpkgs { inherit system; };
+      pkgs = import inputs.nixpkgs { inherit system; overlays = [ inputs.rust-overlay.overlays.default ]; };
+
+      rust = pkgs.rust-bin.stable.latest.default.override {
+        extensions = [ "rust-src" "rust-analyzer" ];
+      };
+
+      crane = (inputs.crane.mkLib pkgs).overrideToolchain rust;
+
+      deps = crane.buildDepsOnly {
+        src = inputs.nix-filter.lib.filter {
+          root = ./indexer;
+          include = [ "Cargo.toml" "Cargo.lock" ];
+        };
+      };
+
+      indexer = crane.buildPackage {
+        cargoArtifacts = deps;
+        src = inputs.nix-filter.lib.filter {
+          root = ./indexer;
+          include = [ "Cargo.toml" "Cargo.lock" "src" ];
+        };
+      };
 
       buildPostsIndex = pkgs.writeScript "build-posts-index" ''
         ${pkgs.tree}/bin/tree content/posts -J > content/posts.json
@@ -24,6 +61,10 @@
       };
     in
     {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [ rust indexer ];
+      };
+
       apps.build-posts-index = mkApp buildPostsIndex;
       apps.serve = mkApp serve;
     });
---
+++ indexer/src/main.rs
@@ -0,0 +1,76 @@
+use gray_matter::engine::YAML;
+use gray_matter::Matter;
+use gray_matter::Pod;
+
+fn main() {
+    let posts_dir = std::fs::read_dir("content/posts").unwrap_or_else(|_| {
+        eprintln!("Couldn't read content/posts directory");
+        std::process::exit(1);
+    });
+
+    match index_posts(posts_dir) {
+        Ok(posts) => println!("{}", posts.trim().to_string()),
+        Err(err) => eprintln!("{}", err),
+    }
+}
+
+fn index_posts(dir: std::fs::ReadDir) -> Result<String, String> {
+    let mut index = "".to_string();
+
+    let matter = Matter::<YAML>::new();
+
+    for entry in dir {
+        let entry = entry.map_err(|err| err.to_string())?;
+        let path = entry.path();
+        if path.is_dir() {
+            let inner_dir = std::fs::read_dir(path).map_err(|err| err.to_string())?;
+            index.push_str(&index_posts(inner_dir)?);
+        } else {
+            if path.extension().unwrap_or_default() == "md" {
+                let file = std::fs::read_to_string(path.clone()).map_err(|err| err.to_string())?;
+
+                let data = matter.parse(file.as_str()).data;
+                if let Some(Pod::Hash(data)) = data {
+                    // Don't index posts with draft: true
+                    if let Some(Pod::Boolean(draft)) = data.get("draft") {
+                        if draft == &true {
+                            continue;
+                        }
+                    }
+
+                    let path = path
+                        .strip_prefix("content/posts/")
+                        .map_err(|err| err.to_string())?
+                        .to_str()
+                        .map_or(Err("invalid path: couldn't convert to str"), |s| Ok(s))?
+                        .split('.')
+                        .next()
+                        .map_or(Err("invalid path: couldn't strip extension"), |s| Ok(s))?;
+
+                    // First index field is the path
+                    index.push_str(&path);
+
+                    // Second index field is the slug, if not present generate one from the path
+                    index.push(':');
+                    if let Some(Pod::String(slug)) = data.get("slug") {
+                        index.push_str(&slug);
+                    } else {
+                        index.push_str(&path.replace("/", "-"));
+                    };
+
+                    // Last fields are the tags
+                    if let Some(Pod::String(tags)) = data.get("tags") {
+                        index.push(':');
+                        index.push_str(&tags.replace(" ", ":"));
+                    };
+
+                    index.push('\n');
+                } else {
+                    println!("Invalid front matter in {}", path.to_str().unwrap());
+                }
+            }
+        }
+    }
+
+    Ok(index)
+}
